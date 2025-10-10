619de4ecd7cbfe557b166b9e7e8896ca3424318c
Jared <jaredlll08@gmail.com>
2025-10-10 16:48:41 -0600

    Port to 1.21.10

--- buildSrc/src/main/kotlin/com/blamejared/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/Versions.kt
@@ -1,13 +1,11 @@
 package com.blamejared
 
 object Versions {
-    const val MOD = "16.0"
+    const val MOD = "17.0"
     const val JAVA = "21"
-    const val MINECRAFT = "1.21.9"
-    const val FABRIC_LOADER = "0.17.2"
-    const val FABRIC = "0.134.0+1.21.9"
-    const val FORGE = "49.0.19" // 1.20.4
-    const val FORGE_LOADER = "[49,)" // 1.20.4
-    const val NEO_FORGE = "21.9.11-beta"
+    const val MINECRAFT = "1.21.10"
+    const val FABRIC_LOADER = "0.17.3"
+    const val FABRIC = "0.135.0+1.21.10"
+    const val NEO_FORGE = "21.10.8-beta"
     const val NEO_FORGE_LOADER= "[4,)"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,5 +17,4 @@ plugins {
 rootProject.name = "bigshot"
 include("common")
 include("fabric")
-//include("forge")
 include("neoforge")
